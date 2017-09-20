0224b7c1da326e594cc034661863814fb0c5f4a2
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-20 16:43:04 +0300

    Added the ui for the date storage.

--- server-test/src/main/java/com/tmattila/ui/DateUI.java
+++ server-test/src/main/java/com/tmattila/ui/DateUI.java
@@ -1,12 +1,19 @@
 package com.tmattila.ui;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 
 @SpringUI(path = "/dateui")
 @Theme("valo")
@@ -14,14 +21,45 @@ import com.vaadin.ui.VerticalLayout;
 public class DateUI extends UI {
 
 	
-	// WORKS 
+	// // WORKS
+	
+	private Label dateLabel;
+	private Label headerLabel;
+	private Date date;
+	private VerticalLayout rootLayout;
+	private Button dateButton;
+	private DateFormat dateF = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	@Override
 	protected void init(VaadinRequest request) {
 
-		VerticalLayout root = new VerticalLayout();
-		root.addComponents(new Label("This is a test 2"));
-		setContent(root);
+		rootLayout = new VerticalLayout();
+		rootLayout.setMargin(true);
+		
+		headerLabel = new Label("Welcome to push button date storage");
+		dateButton = new Button("Click to see the date and time of the button press");
+		dateButton.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				
+				date = new Date();
+				String formattedDate = dateF.format(date);
+				dateLabel = new Label(formattedDate);
+				rootLayout.addComponent(dateLabel);
+				System.out.println(formattedDate);
+				
+			}
+		});
+		
+		rootLayout.addComponent(headerLabel);
+		rootLayout.addComponent(dateButton);
+		setContent(rootLayout);
+		
 	}
 
 }
+
+
+
+
