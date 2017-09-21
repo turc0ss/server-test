0e7e05fe5071ce2886208400148c8114a8ea3670
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-21 11:15:29 +0300

    Improved UI with bolded headers and info text.

--- server-test/src/main/java/com/tmattila/ui/DateUI.java
+++ server-test/src/main/java/com/tmattila/ui/DateUI.java
@@ -22,10 +22,8 @@ import com.vaadin.ui.HorizontalLayout;
 @Title("This is a test")
 public class DateUI extends UI {
 
-	// // WORKS
-
-	private Label dateLabel;
 	private Label headerLabel;
+	private Label dateLabel;
 	private Date date;
 	private VerticalLayout rootLayout;
 	private Button dateButton;
@@ -33,40 +31,42 @@ public class DateUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-
+		
 		rootLayout = new VerticalLayout();
 		rootLayout.setMargin(true);
-
-		VerticalLayout verLayout = new VerticalLayout();
-
-		headerLabel = new Label("<h2><b>Welcome to push button date storage</b></h2><br>", ContentMode.HTML);
-		verLayout.addComponent(headerLabel);
 		
-		VerticalLayout ver2Layout = new VerticalLayout();
-		ver2Layout.setSpacing(true);
-
-		dateButton = new Button("Click to see the date and time of the button press");
-		ver2Layout.addComponent(dateButton);
+		VerticalLayout headerLayout = new VerticalLayout();
 		
+		headerLabel = new Label("<h2><b>Welcome to date storage application</b></h2><br>", ContentMode.HTML);
+		headerLayout.addComponent(headerLabel);
+		
+		VerticalLayout buttonLayout = new VerticalLayout();
+		buttonLayout.setSpacing(true);
+		
+		dateButton = new Button("Click to save the time of button press to database");
 		dateButton.addClickListener(new ClickListener() {
-
+			
 			@Override
 			public void buttonClick(ClickEvent event) {
-
+				
 				date = new Date();
 				String formattedDate = dateF.format(date);
-				dateLabel = new Label(formattedDate);
-				ver2Layout.addComponent(dateLabel);
+				
+				Label timeLabel = new Label("<b>Date and time of button press: </b>", ContentMode.HTML);
+				dateLabel = new Label(timeLabel + formattedDate, ContentMode.HTML);
+				
+				buttonLayout.addComponent(dateLabel);
 				System.out.println(formattedDate);
-
+				
 			}
 		});
-
-		rootLayout.addComponent(verLayout);
-		rootLayout.addComponent(ver2Layout);
-
+		
+		buttonLayout.addComponent(dateButton);
+		
+		rootLayout.addComponent(headerLayout);
+		rootLayout.addComponent(buttonLayout);
+		
 		setContent(rootLayout);
-
 	}
 
 }
