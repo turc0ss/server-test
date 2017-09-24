8fdb136e2ff95e53a143feb889f91acb05662cf8
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-24 20:01:43 +0300

    Fixed checkstyle warnings

--- server-test/src/main/java/com/tmattila/App.java
+++ server-test/src/main/java/com/tmattila/App.java
@@ -5,15 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+/**
+ * 
+ * @author tapsa
+ *
+ */
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
 
+	/**
+	 * @param args
+	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 	
 	@Override
-	protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
 		return builder.sources(App.class);
 	}
 }
