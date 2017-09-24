0ea03d7b12f496093241fd0740693fc7dab8da12
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-24 19:56:44 +0300

    Fixed checkstyle warnings on app parameters

--- server-test/src/main/java/com/tmattila/App.java
+++ server-test/src/main/java/com/tmattila/App.java
@@ -8,12 +8,12 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 	
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
 		return builder.sources(App.class);
 	}
 }
