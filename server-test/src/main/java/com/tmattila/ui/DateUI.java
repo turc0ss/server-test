52382a196cd861466c2d837c46b5e4bdc8c0e4cc
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-30 20:19:16 +0300

    Jenkins deploy test 5

--- server-test/src/main/java/com/tmattila/ui/DateUI.java
+++ server-test/src/main/java/com/tmattila/ui/DateUI.java
@@ -74,7 +74,7 @@ public class DateUI extends UI {
 
 		VerticalLayout headerLayout = new VerticalLayout();
 
-		headerLabel = new Label("<h2><b>Welcome to date storage application JENKINS DEPLOY TEST 4</b></h2><br>", ContentMode.HTML);
+		headerLabel = new Label("<h2><b>Welcome to date storage application JENKINS TEST 5 DEPLOY</b></h2><br>", ContentMode.HTML);
 		headerLayout.addComponent(headerLabel);
 
 		VerticalLayout buttonLayout = new VerticalLayout();
