a6bab5023e85105ce0a8c4c2e367952321286538
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2017-09-30 20:37:21 +0300

    Jenkins automatic staging deploy test

--- server-test/src/main/java/com/tmattila/ui/DateUI.java
+++ server-test/src/main/java/com/tmattila/ui/DateUI.java
@@ -74,7 +74,7 @@ public class DateUI extends UI {
 
 		VerticalLayout headerLayout = new VerticalLayout();
 
-		headerLabel = new Label("<h2><b>Welcome to date storage application JENKINS TEST AFTER ANTIRESOURCELOCKING</b></h2><br>", ContentMode.HTML);
+		headerLabel = new Label("<h2><b>Welcome to date storage application Jenkins TEST AUTOMATICA</b></h2><br>", ContentMode.HTML);
 		headerLayout.addComponent(headerLabel);
 
 		VerticalLayout buttonLayout = new VerticalLayout();
